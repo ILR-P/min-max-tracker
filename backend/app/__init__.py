190b5f5fd80889a99e6f4d57dcf22d96c3c3e426
ILR-P <andor.do.patrick@student.utcluj.ro>
2026-07-09 18:38:05 +0300

    clean up

--- backend/app/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Backend package for the workout tracker API."""
--- frontend/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import './App.css'
-import WorkoutDashboard from './components/WorkoutDashboard'
-
-export default function App() {
-  return <WorkoutDashboard />
-}
